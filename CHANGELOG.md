1c5801636de1fa6314cbfe5a97155e2084fe8555
Yerson Morales <yedeveloper@outlook.com>
2024-09-18 23:09:59 -0500

    Update repo

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Change Log
-
-## [1.0.0] 2023-11-20
-
-### Original Release
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export * from "./navbar";
-export * from "./footer";
-export * from "./layout";
-export * from "./project-card";
-export * from "./resume-item";
-export * from "./skill-card";
-export * from "./fixed-plugin";
